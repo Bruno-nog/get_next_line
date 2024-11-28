7d7edc722ab24f9fc5c1ed6e3565b0821a7d3c1d
Bruno Nogueira De Queiroz <brunogue@c1r6p1.42sp.org.br>
2024-11-28 17:16:58 -0300

    ...

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,255 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 18:41:04 by brunogue          #+#    #+#             */
-/*   Updated: 2024/11/28 16:16:05 by brunogue         ###   ########.fr       */
+/*   Updated: 2024/11/28 17:07:02 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-    static t_fd_node	*head = NULL;
-    t_fd_node			*node;
-    char				buffer[BUFFER_SIZE + 1];
-    ssize_t				bytes_read;
-    char				*line;
+// char	*get_next_line(int fd)
+// {
+//     static t_fd_node	*head = NULL;
+//     t_fd_node			*node;
+//     char				buffer[BUFFER_SIZE + 1];
+//     ssize_t				bytes_read;
+//     char				*line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    node = get_fd_node(&head, fd);
-    if (!node)
-        return (NULL);
-    while (!find_newline(node->remainder))
-    {
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
-        if (bytes_read <= 0)
-            break;
-        buffer[bytes_read] = '\0';
-        node->remainder = ft_strjoin(node->remainder, buffer);
-        if (!node->remainder)
-            return (remove_fd_node(&head, fd), NULL);
-    }
-    if (!node->remainder || *node->remainder == '\0')
-        return (remove_fd_node(&head, fd), NULL);
-    line = extract_line(node->remainder);
-    if (!line)
-        return (NULL);
-    node->remainder = update_remainder(node->remainder);
-    return (line);
+//     if (fd < 0 || BUFFER_SIZE <= 0)
+//         return (NULL);
+//     node = get_fd_node(&head, fd);
+//     if (!node)
+//         return (NULL);
+//     while (!find_newline(node->remainder))
+//     {
+//         bytes_read = read(fd, buffer, BUFFER_SIZE);
+//         if (bytes_read <= 0)
+//             break;
+//         buffer[bytes_read] = '\0';
+//         node->remainder = ft_strjoin(node->remainder, buffer);
+//         if (!node->remainder)
+//             return (remove_fd_node(&head, fd), NULL);
+//     }
+//     if (!node->remainder || *node->remainder == '\0')
+//         return (remove_fd_node(&head, fd), NULL);
+//     line = extract_line(node->remainder);
+//     if (!line)
+//         return (NULL);
+//     node->remainder = update_remainder(node->remainder);
+//     return (line);
+// }
+
+int	get_line_length(t_list *cursor)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	while (cursor != NULL)
+	{
+		i = 0;
+		while (cursor->str[i] != '\0')
+		{
+			if (cursor->str[i] == '\n')
+				return (count);
+			i++;
+			count++;
+		}
+		cursor = cursor->next;
+	}
+	return (count);
 }
 
-// int main(void)
-// {
-// 	get_next_line(0);
-// 	return (0);
-// }
+char	*separate_line(t_list *cursor)
+{
+	char	*line;
+	int		line_length;
+	int		i;
+	int		j;
+
+	line_length = get_line_length(cursor);
+	line = ft_calloc((line_length + 2), 1);
+	if (line == NULL)
+		return (NULL);
+	j = 0;
+	while (cursor != NULL)
+	{
+		i = 0;
+		while (cursor->str[i] != '\n' && cursor->str[i] != '\0')
+			line[j++] = cursor->str[i++];
+		if (cursor->str[i] == '\n')
+		{
+			line[j] = '\n';
+			return (line);
+		}
+		cursor = cursor->next;
+	}
+	return (line);
+}
+
+void	update_next_line(t_list **ptr)
+{
+	t_list	*last;
+	t_list	*new_node;
+	char	*rest_line;
+	int		rest_len;
+	int		i;
+	int		start;
+
+	last = ft_lstlast(*ptr);
+	i = 0;
+	while (last->str[i] != '\n' && last->str[i] != '\0')
+		i++;
+	if (last->str[i] == '\0')
+	{
+		ft_lstclear(ptr);
+		*ptr = NULL;
+		return ;
+	}
+	i++;
+	if (last->str[i] == '\0')
+	{
+		ft_lstclear(ptr);
+		*ptr = NULL;
+		return ;
+	}
+	start = i;
+	rest_len = 0;
+	while (last->str[i] != '\0')
+		rest_len = i++;
+	rest_line = ft_calloc((rest_len + 1), 1);
+	i = 0;
+	while (last->str[start] != '\0')
+	{
+		rest_line[i] = last->str[start++];
+		i++;
+	}
+	new_node = ft_lstnew(rest_line);
+	ft_lstclear(ptr);
+	*ptr = new_node;
+}
+
+int	ft_find_newline(t_list *next_line)
+{
+	t_list	*cursor;
+	int		i;
+
+	cursor = next_line;
+	while (cursor != NULL)
+	{
+		i = 0;
+		while (cursor->str[i] != '\n' && cursor->str[i] != '\0')
+			i++;
+		if (cursor->str[i] == '\n')
+			return (1);
+		cursor = cursor->next;
+	}
+	return (0);
+}
+
+void	read_and_check_newline(int fd, t_list **ptr_next_line,
+		char **ptr_buffer, int bytes_read)
+{
+	t_list	*new_buff_node;
+
+	new_buff_node = NULL;
+	while (!ft_find_newline(*ptr_next_line) && bytes_read > 0)
+	{
+		*ptr_buffer = ft_calloc((BUFFER_SIZE + 1), 1);
+		bytes_read = read(fd, *ptr_buffer, BUFFER_SIZE);
+		if ((!*ptr_next_line && !*ptr_buffer[0] && !bytes_read)
+			|| bytes_read < 0)
+		{
+			free(*ptr_buffer);
+			ft_lstclear(ptr_next_line);
+			*ptr_next_line = NULL;
+			break ;
+		}
+		if (bytes_read == 0)
+		{
+			free(*ptr_buffer);
+			break ;
+		}
+		if (*ptr_next_line == NULL)
+			*ptr_next_line = ft_lstnew(*ptr_buffer);
+		else
+		{
+			new_buff_node = ft_lstnew(*ptr_buffer);
+			ft_lstadd_back(ptr_next_line, new_buff_node);
+			new_buff_node = NULL;
+		}
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	char			*buffer;
+	char			*line;
+	static t_list	*next_line;
+	int				bytes_read;
+
+	bytes_read = 1;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	read_and_check_newline(fd, &next_line, &buffer, bytes_read);
+	if(next_line == NULL)
+		return (NULL);
+	line = separate_line(next_line);
+	update_next_line(&next_line);
+	return (line);
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int i;
+	int fd;
+	char *res;
+
+	i = 0;
+	fd = open("teste.txt", O_RDONLY);
+	// fd = -1;
+	//1
+	res = get_next_line(fd);
+	printf("1: %s", res);
+	free(res);
+	//2
+	res = get_next_line(fd);
+	printf("1: %s", res);
+	free(res);
+
+	close(fd);
+	// fd = open("teste.txt", O_WRONLY);
+	//1
+	res = get_next_line(fd);
+	printf("1: %s", res);
+	free(res);
+	//2
+	res = get_next_line(fd);
+	printf("2: %s", res);
+	free(res);
+	//3
+	res = get_next_line(fd);
+	printf("3: %s", res);
+	free(res);
+	//4
+	res = get_next_line(fd);
+	printf("4: %s", res);
+	free(res);
+	//5
+	res = get_next_line(fd);
+	printf("5: %s", res);
+	free(res);
+	//6
+	res = get_next_line(fd);
+	printf("6: %s", res);
+	free(res);
+} 
+
+
+
 
 
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 18:40:59 by brunogue          #+#    #+#             */
-/*   Updated: 2024/11/28 15:36:03 by brunogue         ###   ########.fr       */
+/*   Updated: 2024/11/28 17:11:16 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct s_fd_node
+
+
+typedef struct c_list
 {
-	int					fd;
-    char				*remainder;
-	struct s_fd_node	*next;
-}	t_fd_node;
+	char			*str;
+	struct c_list	*next;
+}					t_list;
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 8
 #endif
 
-
-char *get_next_line(int fd);
-t_fd_node *get_fd_node(t_fd_node **head, int fd);
-void remove_fd_node(t_fd_node **head, int fd);
-int ft_strlen(const char *str);
-char *ft_strjoin(char *s1, char *s2);
-int find_newline(const char *str);
-char *extract_line(char *remainder);
-char *update_remainder(char *remainder);
+char	*get_next_line(int fd);
+int	get_line_length(t_list *cursor);
+char	*separate_line(t_list *cursor);
+void	update_next_line(t_list **ptr);
+int	ft_find_newline(t_list *next_line);
+void	read_and_check_newline(int fd, t_list **ptr_next_line,
+		char **ptr_buffer, int bytes_read);
+void	ft_lstclear(t_list **lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstnew(char *str);
 void	*ft_calloc(size_t nmemb, size_t size);
+
+
+
+
+// typedef struct s_fd_node
+// {
+// 	int					fd;
+//     char				*remainder;
+// 	struct s_fd_node	*next;
+// }	t_fd_node;
+
+//MEU
+// char *get_next_line(int fd);
+// t_fd_node *get_fd_node(t_fd_node **head, int fd);
+// void remove_fd_node(t_fd_node **head, int fd);
+// int ft_strlen(const char *str);
+// char *ft_strjoin(char *s1, char *s2);
+// int find_newline(const char *str);
+// char *extract_line(char *remainder);
+// char *update_remainder(char *remainder);
+// void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
